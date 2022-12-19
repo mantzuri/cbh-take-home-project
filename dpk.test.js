01e4e09b40e6bfc6ec3d804069270d298798eb9e
Idan Mantzor <mantzor@outlook.com>
2022-12-19 13:51:30 -0700

    add unit tests

--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,16 @@
-const { deterministicPartitionKey } = require("./dpk");
+import { deterministicPartitionKey } from "./dpk";
 
 describe("deterministicPartitionKey", () => {
   it("Returns the literal '0' when given no input", () => {
     const trivialKey = deterministicPartitionKey();
-    expect(trivialKey).toBe("0");
+    expect(trivialKey).toEqual("0");
+  });
+  it("should return a hash of the event if no partition key", () => {
+    const event = { foo: "bar" };
+    expect(deterministicPartitionKey(event).match(/^[a-f0-9]{128}$/));
+  });
+  it("should return hash if the partition key is too long", () => {
+    const event = { partitionKey: "a".repeat(300) };
+    expect(deterministicPartitionKey(event)).toMatch(/^[a-f0-9]{128}$/);
   });
 });
